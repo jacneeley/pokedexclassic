a6ebe7641b4a2883a1155141bfa01e4e0afd80f5
ʒak <102487863+jacneeley@users.noreply.github.com>
2024-09-22 19:45:31 -0500

    tests (#8)

--- src/main/java/local/classicdex/pokedexclassic/app/AppConstants.java
+++ src/main/java/local/classicdex/pokedexclassic/app/AppConstants.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppConstants {
 	private final String pwd = "";
+	private final String ver = "Ver. 0.5";
 
 	public String getPwd() {
 		return pwd;
+	}
+
+	public String getVer() {
+		return ver;
 	}
 }
